2c068ba8872c488f6208557849b010426c1971cc
TimSelivan <timselivanson@gmail.com>
2022-07-01 21:23:52 +0300

    All methods was implemented.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -14,7 +14,7 @@ public class ArrayStorage {
         }
     }
 
-    void save(Resume addResume) {
+    void save(Resume newResume) {
         int count = 0;
         for(int i = count; i < storage.length; i++) {
             if(storage[i] != null) {
@@ -23,22 +23,39 @@ public class ArrayStorage {
                 break;
             }
         }
-        storage[count] = addResume;
+        storage[count] = newResume;
     }
 
     Resume get(String uuid) {
-        return null;
+        int count = 0;
+        for (Resume resume : storage) {
+            count++;
+            if (resume.uuid.equals(uuid)) {
+                break;
+            }
+        }
+        return storage[count];
     }
 
     void delete(String uuid) {
+        int i = 0;
+        for(i = 0; i < storage.length; i++) {
+            if(storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                break;
+            }
+        }
+        System.arraycopy(storage, (i + 1), storage, i, (storage.length - 1 - i));
+        storage[storage.length - 1] = null;
     }
-
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-
-        return new Resume[0];
+        int arrayCopySize = size();
+        Resume[] arrayCopy = new Resume[arrayCopySize];
+        System.arraycopy(storage, 0, arrayCopy, 0, arrayCopySize);
+        return arrayCopy;
     }
 
     int size() {
