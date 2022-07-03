9dd9f9893add5a0d73bbec9e05e1ce28752caabc
TimSelivan <timselivanson@gmail.com>
2022-07-03 20:12:35 +0300

    Methods: clear, save, get and delete were fixed

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,57 +6,54 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        for(int i = 0; i <= size; i++) {
-            if(storage[i] != null) {
-                storage[i] = null;
-                size--;
-            } else {
-                break;
-            }
+        for(int i = 0; i < size; i++) {
+            storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume newResume) {
-        int count = 0;
-        for(int i = count; i < size; i++) {
-            if(storage[i] != null) {
-                count++;
-            } else {
-                break;
-            }
-        }
-        storage[count] = newResume;
-        size += 1;
+        storage[size] = newResume;
+        size++;
     }
 
     Resume get(String uuid) {
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+        int i = 0;
+        boolean contains = false;
+        for(i = 0; i < size; i++) {
+            if(storage[i].uuid.equals(uuid)) {
+                contains = true;
                 break;
             }
-            count++;
         }
-        return storage[count];
+        if(contains) {
+            return storage[i];
+        } else {
+            return null;
+        }
     }
 
     void delete(String uuid) {
         int i = 0;
+        boolean contains = false;
         for(i = 0; i < size; i++) {
             if(storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
-                size--;
+                contains = true;
                 break;
             }
         }
-        System.arraycopy(storage, (i + 1), storage, i, (size - i));
-        storage[size] = null;
+        if(contains) {
+            storage[i] = null;
+            size--;
+            System.arraycopy(storage, (i + 1), storage, i, (size - i));
+        } else {
+            System.out.println("Resume " + uuid + " was not found");
+        }
     }
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-//        int arrayCopySize = size();
         Resume[] arrayCopy = new Resume[size];
         System.arraycopy(storage, 0, arrayCopy, 0, size);
         return arrayCopy;
