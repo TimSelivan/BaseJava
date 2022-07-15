8efbe6a6ddf6ba74e0f12131ed708650d89ccb15
TimSelivan <timselivanson@gmail.com>
2022-07-15 16:57:53 +0300

    HW 4. AbstractArrayStorageTest.java implemented

--- test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
@@ -4,4 +4,8 @@ import static org.junit.Assert.*;
 
 public class ArrayStorageTest extends AbstractArrayStorageTest {
 
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
+
 }
